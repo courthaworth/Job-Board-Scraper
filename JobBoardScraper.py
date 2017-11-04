0a9ce365829ced1f99d20e9f68d04d3048b2994a
Court Haworth <courthaworth@Courts-MBP.zyxel.com>
2017-11-04 15:32:23 -0400

    Added email functionality, updated readme.

---
+++ JobBoardScraper.py
@@ -0,0 +1,56 @@
+#scrape
+#look for data, MAchine learning, " ML "
+#Send email
+
+from bs4 import BeautifulSoup
+import requests
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+url = 'https://news.ycombinator.com/jobs'
+response = requests.get(url)
+html = response.content
+soup = BeautifulSoup(html,'lxml')
+#print(soup.prettify())
+urldict = {}
+keywordlist = ["data", "machine learnning", " ml "]
+for link in soup.find_all('a'):
+	#print(link.find(text=True))
+	body = str(link.find(text=True))
+	if(any(substring in body.lower() for substring in keywordlist)):
+		print(body)
+		if(link.get("href").startswith("item")):
+			urldict[body] = "https://news.ycombinator.com/{}".format(link.get("href"))
+		else: 
+			urldict[body] = link.get("href")
+
+print(urldict)
+
+hostaddress = input("Input Your Host address:")
+portnumber = int(input("Input your port:"))
+
+s = smtplib.SMTP(host=hostaddress, port=portnumber)
+#s = smtplib.SMTP('localhost')
+s.starttls()
+MY_ADDRESS = input("Input Email address that will send email:")
+PASSWORD = input("Input Password to said account: ")
+
+s.login(MY_ADDRESS, PASSWORD)
+
+msg = MIMEMultipart()       # create a message
+
+    # setup the parameters of the message
+msg['From']=MY_ADDRESS
+msg['To']=MY_ADDRESS
+msg['Subject']="Job Postings"
+
+# add in the message body
+for body, url in urldict.items():
+	text = "\n{}:\n{}".format(body,url)
+	msg.attach(MIMEText(text, 'plain'))
+
+# send the message via the server set up earlier.
+s.send_message(msg)
+    
+del msg
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Job-Board-Scraper
 
 This project will be a web scraper that searches a number of job boards including YCombinator's Hacker News in order to find new job postings relevant to data science. The scraper will look for post titles that contain key words like Data, Machine Learning or ML. Once these postings are collected, the script will send an email to your email address containing hyperlinks to those postings.
+
+You will need to input outgoing mail host(ex. smtpauth.earthlink.net) corresponding to the email you wish to use to send the email. You will also need to choose the port number (common port numbers are 25, 465, and 587). After inputting these, you will be asked to input the corresponding email address and password. I am planning on adding the functionality to enter all four of these things as command line arguments so that this script can be automated to run every 24 hours or in a similar manner.
